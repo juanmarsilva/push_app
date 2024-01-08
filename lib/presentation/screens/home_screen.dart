8a51db23ebd0a86e3ffa690f981d46b7e2c1679a
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-07 21:13:03 -0300

    md remove placeholder

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -27,6 +27,11 @@ class _HomeView extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return const Placeholder();
+        return ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+                return const ListTile();
+            },
+        );
     }
 }
