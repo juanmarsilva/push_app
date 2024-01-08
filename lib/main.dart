b4f88b217ec044adf2473ff40f42fda119e8dedb
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-01-07 21:05:44 -0300

    md create and implement router

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
 
 void main() {
@@ -6,18 +7,14 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-    const MainApp({super.key});
+    const MainApp({ super.key });
 
     @override
     Widget build(BuildContext context) {
-        return MaterialApp(
+        return MaterialApp.router(
+            routerConfig: appRouter,
             debugShowCheckedModeBanner: false,
             theme: AppTheme().getTheme(),
-            home: const Scaffold(
-                body: Center(
-                    child: Text('Hello World!'),
-                ),
-            ),
         );
     }
 }
